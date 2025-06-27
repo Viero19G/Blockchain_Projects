01ecc91d72774499cec8656be08014b910858844
Viero19G <gabrielviero22@gmail.com>
2025-06-27 17:58:14 -0300

    dever de casa copiado

---
+++ cursos/rust_101/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+
+    let texto = "Hello World";
+    // Para variavel mudar de valor é necessário declarar com mut
+    let mut idade = 23;
+    //Para corrigir isso bastava uma condicional if por exemplo
+    //final de 2025 terei
+    idade = 24;
+    // Hello world a Lenda do primeiro código
+    println!("{}", texto);
+    println!("Minha idade ao final de 2025 será {}", idade); 
+}
