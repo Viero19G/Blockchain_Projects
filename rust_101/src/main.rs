ea53a54420087ad94ea26fb705f476a55b09dfd6
Viero19G <gabrielviero22@gmail.com>
2025-06-27 16:35:34 -0300

    variáveis RUst

--- rust_101/src/main.rs
+++ rust_101/src/main.rs
@@ -1,4 +1,12 @@
 fn main() {
+
+    let texto = "Hello World";
+    // Para variavel mudar de valor é necessário declarar com mut
+    let mut idade = 23;
+    //Para corrigir isso bastava uma condicional if por exemplo
+    //final de 2025 terei
+    idade = 24;
     // Hello world a Lenda do primeiro código
-    println!("Hello, world!");
+    println!("{}", texto);
+    println!("Minha idade ao final de 2025 será {}", idade);
 }
