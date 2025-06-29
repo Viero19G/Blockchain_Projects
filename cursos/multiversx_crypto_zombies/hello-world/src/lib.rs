391ff333cb4c6e660f17d8af4e94a11f2371a2c7
Viero19G <gabrielviero22@gmail.com>
2025-06-29 20:37:14 -0300

    Uso de arrays e maps

--- cursos/multiversx_crypto_zombies/hello-world/src/lib.rs
+++ cursos/multiversx_crypto_zombies/hello-world/src/lib.rs
@@ -11,10 +11,13 @@ multiversx_sc::derive_imports!();
 // Pensar dessa forma me ajuda a esclarecer que Struct vai otimizar o uso da VM no momento do armazenamento
 // Para String podemos usar ManageBuffer que pode ser chamado no código assim: 
 // let greeting = ManagedBuffer::from(b"Hello world!");
-
+// porém a notação acima resolve apenas se for um único dado.
+// O que fazemos com struct é dizer "O que queremos armazenar tem a seguinte estrutura.... "
+// Adicionamos as notações necessárias 
 pub struct Zombie<M: ManagedTypeApi> {
-  dna: u64,
   name: ManagedBuffer<M>,
+  dna: u64,
+
 }
 
 #[allow(unused_imports)]
@@ -26,6 +29,8 @@ pub trait ZombiesContract {
     #[init]
     fn init(&self) {
     self.dna_digits().set(16u8);
+    // nicializando um contador para o próximo ID de zumbi em 1
+    self.zombie_last_index().set(1usize);
     }
 
     #[upgrade]
@@ -35,4 +40,18 @@ pub trait ZombiesContract {
     // deve ser utilizado chamando SingleValueMapper<aqui_o_tipo_da_variável_rust >
     #[storage_mapper("dnaDigits")]
     fn dna_digits(&self) -> SingleValueMapper<u8>;
+
+    // no armazenamento global do contrato será "zombieLastIndex".
+    // Por que existe: Mapeadores de armazenamento são a forma como os contratos MultiversX
+    // guardam dados de forma persistente, ou seja, dados que sobrevivem após a execução
+    // de uma transação e estão disponíveis para futuras interações com o contrato.
+    #[storage_mapper("zombieLastIndex")]
+    fn zombie_last_index(&self) -> SingleValueMapper<usize>;
+
+    // O que faz: Este é um mapeador de armazenamento configurado para guardar os objetos 'Zombie'.
+    // Diferente de um 'MapMapper' (que armazena múltiplos itens sob uma única chave raiz),
+    // um 'SingleValueMapper' aqui, com um parâmetro 'id', indica que cada Zumbi será
+    // armazenado como um valor único, acessível por uma chave composta que inclui "zombies" e o 'id' fornecido.
+    #[storage_mapper("zombies")]
+    fn zombies(&self, id: usize) -> SingleValueMapper<Zombie<Self::Api>>;
 }
