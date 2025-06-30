afeca6bf2d7f6ce08f1514ba89dfc7f2a1c25c78
Viero19G <gabrielviero22@gmail.com>
2025-06-30 19:54:35 -0300

    criando função random zombie

--- cursos/multiversx_crypto_zombies/hello-world/src/lib.rs
+++ cursos/multiversx_crypto_zombies/hello-world/src/lib.rs
@@ -93,7 +93,8 @@ pub trait ZombiesContract {
     // porque o endpoint altera o estado da blockchain
     #[endpoint]
     fn create_random_zombie(&self, name: ManagedBuffer){
-        
+        let rand_dna = self.generate_random_dna(); // self sempre que for acionar funções do contrato.
+        create_zombie(name, rand_dna) // criando e retornando um novo Zombie com o nome recebido e com um valor dna aleatório
     }
     //----------------------------------------//
 
